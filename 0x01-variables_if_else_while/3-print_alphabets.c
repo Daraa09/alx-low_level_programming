d84dd4f6c7ceb94ea557c50496d696832710f040
Daraa <agbetile@gmail.com>
2023-02-17 20:02:58 -0800

    Numbers

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,6 +6,7 @@
 * followed by a new line
 * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int ch;
@@ -14,5 +15,6 @@ int main(void)
 		putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
+	putchar ('\n');
 	return (0);
 }
