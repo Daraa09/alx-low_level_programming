f8e2bfc5e8dc02098798a65897f4b92d615e9d48
Daraa <agbetile@gmail.com>
2023-02-18 05:56:05 -0800

    Combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,15 @@ int main(void)
 {
 	int n, m;
 
-	for (m = 49; m <= 57; m++)
+	for (n = 48; n <= 56; n++)
 	{
-		if (m > n)
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
 		{
 			putchar(n);
 			putchar(m);
-			{
-				if (n != 56 || m != 57)
+			if (n != 56 || m != 57)
 				{
 					putchar(',');
 					putchar(' ');
